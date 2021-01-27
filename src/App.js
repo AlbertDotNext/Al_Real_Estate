dd2781627e39c12a97033c4a0697f351c653b0e7
Albert Wu <albertwu0001@gmail.com>
2021-01-27 15:57:08 -0800

    styling main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Watsuppp</h1>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route />
+        </Switch>
+      </Router>
     </div>
   );
 }
